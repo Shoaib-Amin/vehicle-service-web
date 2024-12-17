50060cb772730658439f8d88c7a5c8b84ea83dc8
Shoaib Amin <code007program@gmail.com>
2024-12-17 21:43:41 +0500

    next config file change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack(config, { isServer }) {
+    // If necessary, you can create an alias for your src directory
+    if (!isServer) {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
