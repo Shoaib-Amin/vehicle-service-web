5e3d9dfad54422b2a8ff7ee44d7a81e47b5bf691
Shoaib Amin <code007program@gmail.com>
2024-12-18 04:06:04 +0500

    config file change

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
-import { NextConfig } from 'next'
-import path from 'path'
-
-const nextConfig: NextConfig = {
-  webpack(config, { isServer }) {
-    // If necessary, you can create an alias for your src directory
-    if (!isServer) {
-      config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    }
-    return config
+module.exports = {
+  experimental: {
+    appDir: true,
   },
-}
-
-export default nextConfig
+};
